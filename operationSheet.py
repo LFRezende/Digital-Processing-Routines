8a24841fddcca1e1af5bbaaa33b3cec9908796b0
LFRezende <lfsrsprofessional@gmail.com>
2024-03-17 17:48:32 -0300

    Scratches added

--- operationSheet.py
+++ operationSheet.py
@@ -8,10 +8,13 @@ H = 50
 r = 100
 compression = 1
 x = np.arange(-L, L, 1/r)
-step = np.full(shape= L, fill_value= 1)
-s, _ = chirp(x)
-x = np.flip(x)
-h, _ = chirp(x)
+step = np.full(shape= 2*L*r, fill_value= 1)
+s, _ = chirp(x, kf = 1)
+h, _ = chirp(x, kf = 1)
+h = sinc(x**2)
+s = h
+#h = step
+#s = step
 y = convolute(s, h)
 y_length = len(y)
 t = np.arange(-y_length/(2*r), y_length/(2*r), 1/r)
--- operations.py
+++ operations.py
@@ -1,7 +1,8 @@
 import numpy as np
+
 def convolute(s, h):
     y = []
-    h = h #/(len(h)) # Normalize the filter.
+    h = np.flip(h) #/(len(h)) # Normalize the filter.
     max_length = len(s) + len(h) - 1
     while True:
         m = 0
@@ -17,7 +18,30 @@ def convolute(s, h):
             m += 1
             
         # Append the convolution as a signal number.
-        y.append(conv)
+        y.append((1/(max_length))*conv)
+        if len(y) == max_length:
+            break
+    return y
+
+def convolute2(s, h):
+    y = []
+    h = np.flip(h) #/(len(h)) # Normalize the filter.
+    max_length = len(s) + len(h) - 1
+    while True:
+        m = 0
+        conv = 0
+        n = len(y)
+        # Line of Convolution
+        if n >= len(h):
+            h = np.insert(h, 0, 0)  # To correct the size whilst not messing the correlation
+        if n >= len(s):
+            s = np.append(s, 0)
+        while m <= n:
+            conv += h[m]*s[m]
+            m += 1
+            
+        # Append the convolution as a signal number.
+        y.append((1/(max_length))*conv)
         if len(y) == max_length:
             break
     return y
