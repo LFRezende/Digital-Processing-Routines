8099ec0668fa3e3b2e9f4a1cdb144c2311dc489f
LFRezende <lfsrsprofessional@gmail.com>
2024-03-19 15:47:59 -0300

    Update: Convolution now is fast

--- operationSheet.py
+++ operationSheet.py
@@ -2,22 +2,52 @@ import numpy as np
 from signals import *
 from operations import *
 from matplotlib import pyplot as plt
-e = 1
-L = 10
-H = 50
-r = 100
-compression = 1
+
+L = 20
+H = 5
+r = 500
+checkSinc =  False
+checkSweep = True
+
+PSLR_v = 0
+mainlobe = 0
+sidelobe = 0
+strip = 1*r
+
+
+
 x = np.arange(-L, L, 1/r)
 step = np.full(shape= 2*L*r, fill_value= 1)
+
+if checkSweep:
+    sweep_match(x, rate = 0.0001, amount = 200, r = r)
 s, _ = chirp(x, kf = 1)
-h, _ = chirp(x, kf = 1) # Baixo descasamento (~0.5%) AUMENTA o PSLR!!!
-
+h, _ = chirp(x, kf = 1 )# Baixo descasamento (~0.5%) AUMENTA o PSLR!!!
 y = convolute(s, h)
+
+if checkSinc:
+    y = sinc(x)
+
+PSLR_v, mainlobe, sidelobe = PSLR(y, strip)
+
 y_length = len(y)
+
 t = np.arange(-y_length/(2*r), y_length/(2*r), 1/r)
-plt.plot(x, s, label="Signal")
+res, x2, x1, z1, z2 = resolution(y,t,  strip)
 
+print(f"======================\nPSLR dB: {20*np.log10(sidelobe/mainlobe)} dB\nPSLR: {PSLR_v}, {mainlobe} e {sidelobe}\nSize: {res}, {x1} e {x2}\n======================")
+print(f"======================\nResolution: {2*(t[x2] - t[x1])/(t[z1] - t[z2])}, {x1} e {x2}\n======================")
 
+ 
+#plt.plot(x, s, label="Signal")
+plt.vlines(t[int(len(t)/2 - strip/2)], -1, 1, colors = "r", linestyles="dashed")
+plt.vlines(t[int(len(t)/2 + strip/2)], -1, 1, colors = "r", linestyles="dashed")
+plt.vlines(t[int(z2)], -1, 1, colors = "g", linestyles="dotted")
+plt.vlines(t[int(z1)], -1, 1, colors = "g", linestyles="dotted")
+plt.vlines(t[int(x1)], -1, 1, colors = "y", linestyles="dotted")
+plt.vlines(t[int(x2)], -1, 1, colors = "y", linestyles="dotted")
+plt.hlines(mainlobe, -L, L, colors = "r", linestyles = "dashed")
+plt.hlines(sidelobe, -L, L, colors = "r", linestyles = "dashed")
 plt.plot(t, y, label = "Convolution")
 plt.xlabel("Time")
 plt.ylabel("Intensity")
--- operations.py
+++ operations.py
@@ -1,9 +1,51 @@
 import numpy as np
+from signals import *
+from matplotlib import pyplot as plt
+ 
 
-def unmatching_filter_quality():
-    pass
+def sweep_match(x, rate = 0.1, amount = 1, r = 100):
+    s, _ = chirp(x, kf = 1)
+    h = []
+    i = 0
+    pslr_vector = []
+    resol_vector = []
+    kf_vector = []
+    while True:
+        h, _ = chirp(x, kf = 1 + i*rate)
+        y = convolute(s, h)
+
+        y_length = len(y)
+        L = y_length
+        pslr, mainlobe, sidelobe = PSLR(y, y_length)
+        
+        t = np.arange(-y_length/(2*r), y_length/(2*r), 1/r)
+        i = i + 1
+        res, x2, x1, z1, z2 = resolution(y,t, y_length/25)
+        if amount <= 10:
+            plt.plot(t, y, label = f"Kf = {1 + i*rate}")
+            plt.hlines(mainlobe, -y_length/(2*r), y_length/(2*r), colors = "r", linestyles = "dashed")
+            plt.hlines(sidelobe, -y_length/(2*r), y_length/(2*r), colors = "r", linestyles = "dashed")
+            print(f"==================\n Curve {i}:\n PSLR (dB): {20*np.log10(sidelobe/mainlobe)}.\nResolution: {2*(t[x2] - t[x1])/(t[z1] - t[z2])}, {x1} e {x2}\n==================")
+        pslr_vector.append(20*np.log10(sidelobe/mainlobe))
+        resol_vector.append(2*(t[x2] - t[x1])/(t[z1] - t[z2]))
+        kf_vector.append(1 + i*rate)
+        if i == amount:
+            print(f"chegou, i = {i}, amount = {amount}")
+            plt.grid(True)
+            plt.legend()
+            plt.show()
+            plot_performance(kf_vector, pslr_vector, resol_vector)
+            break
+    return (pslr_vector, resol_vector)
 
 def convolute(s, h):
+    y = np.convolve(s, h)
+    length = len(y)
+    return y/length
+ 
+
+def convolution(s, h):
+
     y = []
     # See the convolution in the scratches.txt to understand this.
     limit_length = (len(s) + len(h) - 1)  # See "scratches"
@@ -29,39 +71,79 @@ def convolute(s, h):
         # Now add at the beg and the end
         s = np.append(s, 0)
         h = np.insert(h, 0, 0)
-       
         #print(conv)
         y.append(conv)
-        
         # Append the convolution as a signal number.
     return y
 
-'''def convolute2(s, h):
-    y = []
-    h = np.flip(h) #/(len(h)) # Normalize the filter.
-    max_length = len(s) + len(h) - 1
-    while True:
-        m = 0
-        conv = 0
-        n = len(y)
-        # Line of Convolution
-        if n >= len(h):
-            h = np.insert(h, 0, 0)  # To correct the size whilst not messing the correlation
-        if n >= len(s):
-            s = np.append(s, 0)
-        while m <= n:
-            conv += h[m]*s[m]
-            m += 1
-            
-        # Append the convolution as a signal number.
-        y.append((1/(max_length))*conv)
-        if len(y) == max_length:
-            break
-    return y'''
+def PSLR(s, L):
+    central_strip = int(L/2)
+    max1 = s[0]
+    max2 = s[0]
 
-    
-    
+    for k, v in enumerate(s):
+        if k < len(s) - 1:
+            if k > len(s)/2 - central_strip and k < len(s)/2 + central_strip:
+                 if v > max1 and v > s[k+1]:
+                    max2 = max1
+                    max1 = v
 
+
+    if max2 != 0:
+        PSLR_Value = max1/max2
+    else:
+        PSLR_Value = 0
+    return (PSLR_Value,max1, max2)
+
+
+def resolution(s, t,  L):
+    const = 0.7071
+    x1 = x2 = 0
+    z1 = z2 = 0
+    firstZero = False
+    length = len(s)
+    _, mlobe, slobe = PSLR(s, L)
+
+    for k, v in enumerate(s):
+        if k > (length - L)/2 and k < (length + L)/2:
+            if v < const*mlobe and s[k+1] > const*mlobe:
+                x1 = k + 1
+            if v > const*mlobe and s[k+1] < const*mlobe:
+                x2 = k
+            if v > 0 and s[k+1] < 0 and  not firstZero:
+                z1 = k
+                if t[z1] > 0:
+                    firstZero = True
+
+            if v < 0 and s[k+1] > 0 and not firstZero:
+                z2 = k
+
+
+    return 2*(x2 - x1), x2, x1, z1, z2
 
 def chirp_compression():
     pass
+
+
+def plot_performance(kf, pv, rv):
+    plt.plot(kf, pv, label="PSLR")
+    plt.xlabel("Chirp Rate Ratio")
+    plt.ylabel("Value")
+    plt.title("Change in PSLR")
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+    plt.plot(kf, rv, label="Resolution")
+    plt.xlabel("Chirp Rate Ratio")
+    plt.ylabel("Resolution")
+    plt.title("Change in Resolution")
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+ 
+
+
+'''L = 5
+r = 200
+x = np.arange(-L, L, 1/r)
+sweep_match(x, -0.005, 3)'''
